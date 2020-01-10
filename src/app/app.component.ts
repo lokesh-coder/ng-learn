d5baf0f0ae5463c753c5144d47d3fb4972ed009a
Lokesh r <lokeshr@IN-Lokesh.local>
2020-01-11 04:47:07 +0530

    remove excess boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  template: "<h1>Hello world!</h1>"
 })
-export class AppComponent {
-  title = 'ng-learn';
-}
+export class AppComponent {}
