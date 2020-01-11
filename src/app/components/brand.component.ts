ecbd523ae62d70c58a52275e0e0194e904bcc733
lokesh-coder <mexican.dirtyfellow@gmail.com>
2020-01-11 05:43:49 +0530

    style brand component - end

--- src/app/components/brand.component.ts
+++ src/app/components/brand.component.ts
@@ -3,18 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-brand",
   template: `
-    <h1>Ng Learn</h1>
+    <h1 class="navbar-brand" [style.color]="'#333'">Ng Learn</h1>
   `
 })
 export class BrandComponent {}
-
-/*
-
-    <h1 [class.foobar]="true" [class]="variable">title</h1>
-    <h1 [ngClass]="{ foobar: true, baz: false }">title</h1>
-    <h1 [class.foobar]="true">title</h1>
-    <h1 [style.color]="'red'">title</h1>
-    <h1 [style.fontSize.px]="12">title</h1>
-    <h1 [style.fontSize.%]="200">title</h1>
-
-*/
