94c8dbf48e66f8c4fb84019582917a82ad1bd289
lokesh-coder <mexican.dirtyfellow@gmail.com>
2020-01-11 06:05:29 +0530

    login component - start

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, EventEmitter, Output } from "@angular/core";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.template.html"
+})
+export class LoginComponent {
+  // logic
+}
+
+/* dont forget to include it in module */
